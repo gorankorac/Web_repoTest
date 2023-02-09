7cc12b6949be640f9ac147ff4e83683305412dad
gorankorac <gorankorac@gmail.com>
2023-02-09 11:37:40 +0100

    JSON

---
+++ Ajax/JSON/main.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var btn = document.querySelector('.dog');
+var img = document.querySelector('.img')
+
+var xhr = new XMLHttpRequest
+
+
+function request() {
+
+  xhr.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      var data = JSON.parse(xhr.responseText)
+          console.log(data);
+
+      var photo = data.message;
+          console.log(photo);
+
+      img.setAttribute('src', photo)
+    }
+  }
+   
+  xhr.send();
+
+}
+
+btn.addEventListener('click', request)
